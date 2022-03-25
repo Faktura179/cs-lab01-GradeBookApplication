26729bf596141220e3ef4a93395d43e0c66757da
Faktura179 <rfatula12@gmail.com>
2022-03-25 21:11:32 +0100

    Override GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -11,6 +12,28 @@ namespace GradeBook.GradeBooks
             Type = Enums.GradeBookType.Ranked;
         }
 
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
 
+            List<double> grades = Students.SelectMany(x => x.Grades).ToList();
+            //grades.Add(averageGrade);
+
+            grades = grades.OrderByDescending(x => x).ToList();
+
+            double percentile = grades.IndexOf(averageGrade) / (double)grades.Count * 100;
+
+            if (percentile < 20)
+                return 'A';
+            else if (percentile < 40)
+                return 'B';
+            else if (percentile < 60)
+                return 'C';
+            else if (percentile < 80)
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
